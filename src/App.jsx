3c480773ccb27af0de0a01e3bfbf50593d400c70
tisaacnola <oisaac47@gmail.com>
2024-09-30 11:24:22 -0500

    built out homepage and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
-import "./App.css";
+import HomePage from "./components/HomePage";
+import Header from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div></div>;
+  return (
+    <div className="flex flex-col max-w-[1000px] mx-auto w-full">
+      <section className="min-h-screen flex flex-col">
+        <Header />
+        <HomePage />
+      </section>
+      <h1 className="text-green-400">Hello</h1>
+      <footer></footer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 text-center justify-center pb-20">
+      <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
+        Free<span className="text-blue-400 bold">Scribe</span>
+      </h1>
+      <h3 className="font-medium text-lg">
+        Record <span className="text-blue-400">&rarr;</span> Transcribe{" "}
+        <span className="text-blue-400">&rarr;</span> Translate
+      </h3>
+      <button className="flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4">
+        <p className="text-blue-400">Record</p>
+        <i className="fa-solid fa-microphone"></i>
+      </button>
+      <p className="text-base">
+        Or{" "}
+        <label className="text-blue-400 cursor-pointer hover:text-blue-600 duration-200">
+          upload <input className="hidden" type="file" accept=".mp3,.wave" />
+        </label>
+        a mp3 file
+      </p>
+      <p className="italic text-slate-400">Free Now Free Forever</p>
+    </main>
+  );
+}
